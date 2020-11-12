504c024ed1c231f804980e0cc8453b4b1d3a71d3
TianleYou <tianleyou@foxmail.com>
2020-11-12 23:37:19 +0800

    函数注释规范

---
+++ JavaScript/js/函数注释规范.js
@@ -0,0 +1,39 @@
+/**
+ * 说明 -- 函数的作用
+ * 常用的属性
+ * @todo 代办事项
+ * @param {类型} 参数名 描述
+ * @return {类型} 描述
+ */
+(function showNotes () {
+  console.log(`
+    @api: 提供给第三方使用的接口
+    @author: 标明作者
+    @param: 参数
+    @return: 返回值
+    @todo: 待办
+    @version: 版本号
+    @inheritdoc: 文档继承
+    @property: 类属性
+    @property-read: 只读属性
+    @property-write: 只写属性
+    @const: 常量
+    @deprecated: 过期方法
+    @example: 示例
+    @final: 标识类是终态, 禁止派生
+    @global: 指明引用的全局变量
+    @static: 标识类、方法、属性是静态的
+    @ignore: 忽略
+    @internal: 限内部使用
+    @license: 协议
+    @link: 链接,引用文档等
+    @see: 与 link 类似, 可以访问内部方法或类
+    @method: 方法
+    @package: 命名空间
+    @since: 从指定版本开始的变动
+    @throws: 抛出异常
+    @uses: 使用
+    @var: 变量
+    @copyright: 版权声明
+  `);
+})();
